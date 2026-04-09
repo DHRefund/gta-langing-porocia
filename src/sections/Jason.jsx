9b4aa5fdcc0f8f99c743c436aa4fee5174407864
PhamTinh <tinh1235a@gmail.com>
2026-04-09 09:17:58 +0900

    commit v2

--- src/sections/Jason.jsx
+++ src/sections/Jason.jsx
@@ -1,35 +1,48 @@
-import gsap from "gsap"
-import { useGSAP } from "@gsap/react"
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 const Jason = () => {
   useGSAP(() => {
-    gsap.set('.jason', { marginTop: '-80vh' });
+    gsap.set(".jason", { marginTop: "-80vh" });
 
-    gsap.timeline({
-      scrollTrigger: {
-        trigger: '.jason',
-        start: 'top 90%',
-        end: '10% center',
-        scrub: 2,
-      }
-    }).to('.first-vd', { opacity: 0, duration: 1, ease: 'power1.inOut' });
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: ".jason",
+          start: "top 90%",
+          end: "10% center",
+          scrub: 2,
+        },
+      })
+      .to(".first-vd", { opacity: 0, duration: 1, ease: "power1.inOut" });
 
-    gsap.to('.jason .img-box', {
-      scrollTrigger: {
-        trigger: '.jason',
-        start: 'top center',
-        end: '80% center',
-        scrub: 2
-      }, y: -300, duration: 1, ease: 'power1.inOut'
-    }, '<')
-  }) 
+    gsap.to(
+      ".jason .img-box",
+      {
+        scrollTrigger: {
+          trigger: ".jason",
+          start: "top center",
+          end: "80% center",
+          scrub: 2,
+        },
+        y: -300,
+        duration: 1,
+        ease: "power1.inOut",
+      },
+      "<",
+    );
+  });
 
   return (
     <section className="jason">
       <div className="max-w-lg jason-content">
         <h1>Jason Duval</h1>
-        <h2>Jason wants an easy life, but things just keep getting harder.</h2>
-        <p>Jason grew up around grifters and crooks. After a stint in the Army trying to shake off his troubled teens, he found himself in the Keys doing what he knows best, working for local drug runners. It might be time to try something new.</p>
+        {/* <h2>Jason wants an easy life, but things just keep getting harder.</h2>
+        <p>Jason grew up around grifters and crooks. After a stint in the Army trying to shake off his troubled teens, he found himself in the Keys doing what he knows best, working for local drug runners. It might be time to try something new.</p> */}
+        <h2>楽な人生を望むジェイソン。だが、すべてがますます困難になっていく。</h2>
+        <p>
+          ジェイソンは詐欺師や悪党に囲まれて育つ。傷ついた十代を払拭するため軍隊に入隊したが、結局はキーズで自分が得意なことをやっていた。地元の麻薬密売人のために働いていたのだ。そろそろ新しい道を歩む時が来たのかもしれない。
+        </p>
 
         <div className="jason-2">
           <img src="/images/jason-2.webp" />
@@ -45,7 +58,7 @@ const Jason = () => {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Jason
+export default Jason;
--- src/sections/Lucia.jsx
+++ src/sections/Lucia.jsx
@@ -3,25 +3,34 @@ import { useGSAP } from "@gsap/react";
 
 const Lucia = () => {
   useGSAP(() => {
-    gsap.set('.lucia-life', { marginTop: '-80vh'});
+    gsap.set(".lucia-life", { marginTop: "-80vh" });
 
-    gsap.timeline({
-      scrollTrigger: {
-        trigger: '.lucia-life',
-        start: 'top 80%',
-        end: '10% center',
-        scrub: 2,
-      }
-    }).to('.second-vd', { opacity: 0, duration: 1, ease: 'power1.inOut' });
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: ".lucia-life",
+          start: "top 80%",
+          end: "10% center",
+          scrub: 2,
+        },
+      })
+      .to(".second-vd", { opacity: 0, duration: 1, ease: "power1.inOut" });
 
-    gsap.to('.lucia-life .img-box', {
-      scrollTrigger: {
-        trigger: '.lucia-life',
-        start: 'top center',
-        end: '80% center',
-        scrub: 2
-      }, y: -200, duration: 1, ease: 'power1.inOut'
-    }, '<')
+    gsap.to(
+      ".lucia-life .img-box",
+      {
+        scrollTrigger: {
+          trigger: ".lucia-life",
+          start: "top center",
+          end: "80% center",
+          scrub: 2,
+        },
+        y: -200,
+        duration: 1,
+        ease: "power1.inOut",
+      },
+      "<",
+    );
   });
 
   return (
@@ -38,18 +47,29 @@ const Lucia = () => {
       <div className="lg:w-1/2 lucia-life-content">
         <div className="max-w-xl lg:ps-32 ps-10">
           <h1>Lucia Caminos</h1>
-          <h2>Lucia’s father taught her to fight as soon as she could walk.</h2>
-          <p>Life has been coming at her swinging ever since. Fighting for her family landed her in the Leonida Penitentiary. Sheer luck got her out. Lucia’s learned her lesson — only smart moves from here.</p>
+          {/* <h2>Lucia’s father taught her to fight as soon as she could walk.</h2>
+          <p>Life has been coming at her swinging ever since. Fighting for her family landed her in the Leonida Penitentiary. Sheer luck got her out. Lucia’s learned her lesson — only smart moves from here.</p> */}
+          <h2>歩けるようになったその時から、父の教えは一つ──「戦え」</h2>
+          <p>
+            以来、人生は容赦ない連撃をくり出す。家族を守った行動はレオニダ刑務所へ。ただの幸運が脱獄の道を開いた。ルシアはそこで学んだ──本物の強さは、冷徹な判断力だと。
+          </p>
         </div>
 
         <div className="lucia-2">
           <img src="/images/lucia-2.webp" />
         </div>
 
-        <p className="max-w-xl lg:ps-32 ps-10">More than anything, Lucia wants the good life her mom has dreamed of since their days in Liberty City — but instead of half-baked fantasies, Lucia is prepared to take matters into her own hands.</p>
+        <p className="max-w-xl lg:ps-32 ps-10">
+          何より以上に、ルシアはLiberty
+          Cityにいた時代から母が夢見た良い人生を望んでいる。だが、生半可な幻想ではなく、ルシアは自分で人生を切り開く覚悟がある。
+        </p>
+        {/* <p className="max-w-xl lg:ps-32 ps-10">
+          More than anything, Lucia wants the good life her mom has dreamed of since their days in Liberty City — but
+          instead of half-baked fantasies, Lucia is prepared to take matters into her own hands.
+        </p> */}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Lucia
+export default Lucia;
